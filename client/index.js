4c2578587fea44c8a05d33eaec6835df6b060c2e
zimrick <richard@zcreativelabs.com>
2019-11-08 22:27:59 +0100

    Updated project structure to use next js and react hooks

--- client/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react"
-import ReactDOM from "react-dom"
-
-import WorldMap from "./components/WorldMap"
-
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<WorldMap />, document.getElementById("app"))
-})
